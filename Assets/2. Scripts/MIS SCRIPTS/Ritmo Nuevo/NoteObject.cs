51df2d5ec17eaea16b104bdc93f14944edab9bb9
Glucasl <glucasl@esimurcia.com>
2023-03-22 02:18:56 +0800

    Juego de ritmo inacabado
    El juego me duplica el Hit correcto con el fallido

---
+++ Assets/2. Scripts/MIS SCRIPTS/Ritmo Nuevo/NoteObject.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+
+    public KeyCode keyToPress;
+
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (canBePressed)
+            {
+                gameObject.SetActive(false);
+
+                //GameManagerRitmo.instance.NoteHit();
+
+                if(Mathf.Abs (transform.position.y) > 0.25f)
+                {
+                    Debug.Log("Hit");
+                    GameManagerRitmo.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }
+                else if(Mathf.Abs(transform.position.y) > 0.05f)
+                {
+                    Debug.Log("Good");
+                    GameManagerRitmo.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                }
+                else 
+                {
+                    Debug.Log("Perfect");
+                    GameManagerRitmo.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+
+            GameManagerRitmo.instance.NoteMissed();
+            Instantiate(missEffect, transform.position, missEffect.transform.rotation);
+        }
+    }
+}
