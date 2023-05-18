c3f25acd55f420ee515ec419b60b56e6255f8ba5
Glucasl <glucasl@esimurcia.com>
2023-05-19 00:27:35 +0800

    Juego ritmo biennnnnnnnn
    Arreglo todo juego de ritmo

--- Assets/2. Scripts/MIS SCRIPTS/Ritmo Nuevo/NoteObject.cs
+++ Assets/2. Scripts/MIS SCRIPTS/Ritmo Nuevo/NoteObject.cs
@@ -4,42 +4,21 @@ using UnityEngine;
 
 public class NoteObject : MonoBehaviour
 {
-    public bool canBePressed;
+    public bool canBePressed, pressed;
 
     public KeyCode keyToPress;
 
     public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
+    Vector3 lugar;
+
 
     // Update is called once per frame
     void Update()
     {
         if (SimpleInput.GetKeyDown(keyToPress))
         {
-            if (canBePressed)
-            {
-                gameObject.SetActive(false);
-
-                //GameManagerRitmo.instance.NoteHit();
-
-                if(Mathf.Abs (transform.position.y) > 0.25f)
-                {
-                    Debug.Log("Hit");
-                    GameManagerRitmo.instance.NormalHit();
-                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
-                }
-                else if(Mathf.Abs(transform.position.y) > 0.05f)
-                {
-                    Debug.Log("Good");
-                    GameManagerRitmo.instance.GoodHit();
-                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
-                }
-                else 
-                {
-                    Debug.Log("Perfect");
-                    GameManagerRitmo.instance.PerfectHit();
-                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
-                }
-            }
+            presionoTecla();
         }
     }
 
@@ -49,16 +28,48 @@ public class NoteObject : MonoBehaviour
         {
             canBePressed = true;
         }
+
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D dos)
     {
-        if (other.tag == "Activator")
+        if (dos.tag == "Limit")
         {
-            canBePressed = false;
 
+            canBePressed = false;
+            lugar = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
             GameManagerRitmo.instance.NoteMissed();
-            Instantiate(missEffect, transform.position, missEffect.transform.rotation);
+            Instantiate(missEffect, lugar, missEffect.transform.rotation);
+        }
+    }
+
+    public void presionoTecla()
+    {
+
+        if (canBePressed)
+        {
+            gameObject.SetActive(false);
+
+            //GameManagerRitmo.instance.NoteHit();
+
+            if (Mathf.Abs(transform.position.y) > 0.25f)
+            {
+                Debug.Log("Hit");
+                GameManagerRitmo.instance.NormalHit();
+                Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+            }
+            else if (Mathf.Abs(transform.position.y) > 0.05f)
+            {
+                Debug.Log("Good");
+                GameManagerRitmo.instance.GoodHit();
+                Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+            }
+            else
+            {
+                Debug.Log("Perfect");
+                GameManagerRitmo.instance.PerfectHit();
+                Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+            }
         }
     }
 }
