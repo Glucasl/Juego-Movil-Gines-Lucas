e29338a5743642f3be63164f053ddf07b0b9b8ba
Glucasl <glucasl@esimurcia.com>
2023-05-19 01:29:50 +0800

    Correccion segundo nivel
    -Ampliacion de juego parejas
    -Aumento de dificultada en comida
    -Creacion de score bonita

---
+++ Assets/2. Scripts/MIS SCRIPTS/Cambios Escena/CambioComidaDIA2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CambioComidaDIA2 : MonoBehaviour
+{
+    [SerializeField] private GameObject Transicion;
+    [SerializeField] private GameObject BlackScreen;
+    [SerializeField] private float wait = 2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Pcomida();
+        }
+    }
+
+    public void Pcomida()
+    {
+        StartCoroutine(PasarComida());
+    }
+
+    public IEnumerator PasarComida()
+    {
+        Transicion.SetActive(true);
+        BlackScreen.SetActive(true);
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(wait);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(13);
+    }
+}
